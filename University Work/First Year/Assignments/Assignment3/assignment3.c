075d459f73e3c8bbeacfc7c0b8d26e357d915f6f
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-04-04 01:37:38 +0100

    Started assignment 3 and implemented basic functionality
    This includes the main program loop, the function signiture and structures, the end function and error checking on inputs.

---
+++ University Work/First Year/Assignments/Assignment3/assignment3.c
@@ -0,0 +1,147 @@
+/*
+	Description
+
+	Author
+Paul Geoghegan
+	Date
+Started 04/04/20
+Finished x/04/20
+*/
+
+//header files
+#include <stdio.h>
+#include <stdlib.h>
+
+//symbolic names
+#define LENGTH 4
+
+//structures
+struct code_counter
+{
+	int correct_code; 
+	int wrong_code;
+};
+
+//function signature
+void enterNumbers(short[]);
+void incryptNumbers(short[]);
+void compairNumbers(short[]);
+void decryptNumbers(short[]);
+void entryAttempts(struct code_counter attempts);
+void end();
+
+void main()
+{
+
+	short option;
+	short userCode[LENGTH];
+	char str[1];
+	struct code_counter attempts;
+
+	//this is the main loop for looping continiously through the program untill the user chooses to end the program
+	do
+	{
+
+		//this prints the main menu for the program
+		printf("Please choose from one of the following options\n");
+
+		//this gets a value from the user and error checks it so that the user can choose an option
+		do
+		{
+
+			//this gets an input from the user
+			scanf("%s", str);
+
+			//this converts the character that the user entered in to a number corosponding with its ascii value
+			option = atoi(str);
+
+			//this checks if the value entered by the user is valid
+			if(option < 1 || option > 6)
+			{
+
+				printf("Invalid input, Please enter a value between 1 and 6\n");
+
+			} //end if
+
+		} //end do while
+		while(option < 1 || option > 6);
+		while(option < 1 || option > 6);
+
+		if(option == 1)
+		{
+
+	}	 //end if for option 1
+		else if(option == 6)
+	{	
+
+			end();
+
+		} //end else if for option 6
+
+	} //end do while
+	while(1);
+
+} //end main
+
+
+//this function allows the user to end the program
+void end()
+{
+
+	char choose;
+
+	//asks the user if they want to end the program
+	printf("Are you sure you want to end the program?\nY: yes\nN: no\n");
+
+	//gets an input from the user and error checks it
+	do
+	{
+
+		//gets an input from the user
+		scanf("%1s", &choose);
+
+		//checks to see if the user entered a valid input
+		if(choose != 'n' && choose != 'N' && choose != 'y' && choose != 'Y')
+		{
+
+			printf("Invalid input, Please either enter y to exit the program or n to resume the program\n");
+
+		} //end if
+
+	} //end do while
+	while(choose != 'n' && choose != 'N' && choose != 'y' && choose != 'Y');
+
+	//checks if the user wants to exit the program
+	switch(choose)
+	{
+
+		//this case exicutes if the user wishes to exit the program
+		case 'y':
+		case 'Y':
+		{
+
+			printf("Exiting program...\n");
+			exit(0);
+			break;
+
+		} //end yes case
+		//this case exicutes if the user wishes to continue the program
+		case 'n':
+		case 'N':
+		{
+
+			printf("Resumeing program\n");
+			break;
+
+		} //end no case
+		//this case exicutes if something goes wrong
+		default:
+		{
+
+			printf("Something has gone wrong please try again\n");
+
+		} //end default case
+
+	} //end switch case
+
+} //end end function
