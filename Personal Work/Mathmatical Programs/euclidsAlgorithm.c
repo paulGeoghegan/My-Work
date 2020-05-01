10bbafcd8d9dfe334189f713765a253aa8db6d37
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-05-01 16:50:22 +0100

    Created the file for Euclids Algorithm
    This program will use euclids algorithm part1 and part 2 to find GCD, x and y.
    For more info check the program description

---
+++ Personal Work/Mathmatical Programs/euclidsAlgorithm.c
@@ -0,0 +1,27 @@
+/*
+	Description
+This program uses Euclids Algorithm to find the greatest common denominator (GCD) of 2 numbers and also finds x and y so that gcd = x(n1) - y(n2)
+	Author
+Paul Geoghegan
+	Date
+Started 01/05/20
+*/
+
+//header files
+#include <stdio.h>
+
+//symbolic names
+
+
+//structures
+
+
+//function signatures
+
+
+void main()
+{
+
+
+
+} //end main
