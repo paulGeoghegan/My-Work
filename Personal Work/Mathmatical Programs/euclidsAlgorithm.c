9e3a79e85f847ca56688110243310472ec63cfc6
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-05-01 23:43:57 +0100

    Added basic outline of part one and a loop with working stop condition

--- Personal Work/Mathmatical Programs/euclidsAlgorithm.c
+++ Personal Work/Mathmatical Programs/euclidsAlgorithm.c
@@ -4,11 +4,12 @@ This program uses Euclids Algorithm to find the greatest common denominator (GCD
 	Author
 Paul Geoghegan
 	Date
-Started 01/05/20
+01/05/20
 */
 
 //header files
 #include <stdio.h>
+#include <stdlib.h>
 
 //symbolic names
 
@@ -17,11 +18,47 @@ Started 01/05/20
 
 
 //function signatures
-
+long* part1(long*, long*);
 
 void main()
 {
 
+	long gcd, num1, num2;
+	long *numbers;
 
+	//allocates memory for numbers which will be the main block of memory used throught the program
+	numbers = calloc(4, sizeof(long));
+
+	//this is temporary
+	printf("Enter the 2 numbers\n");
+
+	//puts num1 and num2 in the numbers memory block
+	scanf("%ld", &*(numbers+0));
+	scanf("%ld", &*(numbers+2));
+
+	//goes to the part 1 function
+	numbers = part1(numbers, &gcd);
+
+	free(numbers);
 
 } //end main
+
+
+//this function handles the first part of euclids algorithm to find the gcd
+long* part1(long *numbers, long *gcd)
+{
+
+	int i = 1;
+
+	//this loop will continue untill the gcd is found
+	do
+	{
+
+		//tells the user what line it is
+		printf("Line %d\n", i);
+
+	} // end do while
+	while(*(numbers+((i*4)-1)) != 0);
+
+	return numbers;
+} // end part1
