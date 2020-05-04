2735019b7813cedab9caed42c3fcf08e59f816de
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-05-04 23:40:42 +0100

    Included utility functions for: error checking, screen clearing and ending the program

--- Personal Work/Mathmatical Programs/euclidsAlgorithm.c
+++ Personal Work/Mathmatical Programs/euclidsAlgorithm.c
@@ -10,6 +10,8 @@ Paul Geoghegan
 //header files
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <string.h>
 
 //symbolic names
 #define LENGTH 4
@@ -19,6 +21,11 @@ Paul Geoghegan
 
 //function signatures
 long* part1(long*, long*);
+
+//utility functions
+void end();
+long errorCheck(char[], long*, long*);
+void clear();
 
 void main()
 {
@@ -113,3 +120,121 @@ long* part1(long *numbers, long *gcd)
 
 	return numbers;
 } // end part1
+
+
+
+/*
+NOTE: These following functions have been included purely for the sake of improveing usability and serve no function for euclids algorithm
+*/
+
+//this function allows the user to end the program
+void end()
+{
+
+	long min = 1;
+	long max = 2;
+	char str[2];
+
+	//asks the user if they want to end the program
+	printf("Are you sure you want to end the program?\n1: yes\n2: no\n");
+
+	//gets an input from the user and checks what they want to do
+	if(errorCheck(str, &min, &max) == 1)
+	{
+
+		//tells the user that the program is stopping
+		printf("Exiting program...");
+
+		//exits the program
+		exit(0);
+
+		} //end if
+		else
+		{
+
+			//lets the user know that the program is not exiting
+			printf("Resuming program\n");
+
+		} //end else
+
+} //end end function
+
+
+//this function error checks any input from the user
+long errorCheck(char str[], long *min, long *max)
+{
+
+	long num;
+
+	//checks to see if the min value required is < 1 as this requires slightly different logic
+	if(*min < 1)
+	{
+
+		//this gets an input from the user and error checks it
+		do
+		{
+
+			//gets the input from the user and then clears the input buffer
+			scanf("%1s", str);
+			fflush(stdin);
+
+			//checks to see if the input is valid
+			//the last statement is used as if a non numeric char is converted to an int it will return 0 so this last statement checks for the user entering a 0 as otherwise this would not work as intended
+			if((atoi(str) < *min || atoi(str) > *max || atoi(str) == 0) && strcmp(str, "0") != 0)
+			{
+
+				printf("Invalid input please enter a value from %d to %d\n", *min, *max);
+
+			} //end if
+
+		} //end do while also see note above
+		while((atoi(str) < *min || atoi(str) > *max || atoi(str) == 0) && strcmp(str, "0"));
+
+	} //end if
+	else
+	{
+
+		//this gets an input from the user and error checks it
+		do
+		{
+
+			//gets the input from the user and then clears the input buffer
+			scanf("%s", str);
+			fflush(stdin);
+
+			//checks to see if the input is valid
+			if(atoi(str) < *min || atoi(str) > *max)
+			{
+
+				printf("Invalid input please enter a value from %d to %d\n", *min, *max);
+
+			} //end if
+
+		} //end do while
+		while(atoi(str) < *min || atoi(str) > *max);
+
+	} //end else
+
+	// returns num
+	return num = atoi(str);
+
+} //end errorCheck
+
+
+//this function will clear the screen at the end of each of the functions
+void clear()
+{
+
+	//this tells the user to press the enter key to continue
+	printf("Press enter to continue\n");
+
+	//this takes an input from the user but doesnt store it anywhere
+	getchar();
+
+	//this clears the screen
+	system("CLS");
+
+	//delays the program to fix an accessibility issue with clearing the screen in a terminal
+	Sleep(1000);
+
+} //end clear function
