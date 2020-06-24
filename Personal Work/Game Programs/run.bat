5b8666ffa9dec3adc99e98ba4e8585bc6dbb455c
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-06-24 21:07:41 +0100

    Changed tun.bat to name the exe game.exe instead of it being the default a.exe and updated the instructions.md file

--- Personal Work/Game Programs/run.bat
+++ Personal Work/Game Programs/run.bat
@@ -1,3 +1,3 @@
 @ECHO OFF 
-gcc character.c inventory.c map.c menu.c play.c save.c tiles.c utility.c
-a.exe
+gcc character.c inventory.c map.c menu.c play.c save.c tiles.c utility.c -o game.exe
+game.exe
