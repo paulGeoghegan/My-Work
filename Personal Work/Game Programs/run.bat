55c2e85668741694dbd65dfad0d20382ed122dfa
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-07-03 21:33:58 +0100

    Decided on trialsByText as the game name

--- Personal Work/Game Programs/run.bat
+++ Personal Work/Game Programs/run.bat
@@ -1,3 +1,3 @@
 @ECHO OFF 
-gcc admin.c character.c inventory.c items.c map.c menu.c play.c save.c tiles.c utility.c -o game.exe
-game.exe
+gcc admin.c character.c inventory.c items.c map.c menu.c play.c save.c tiles.c utility.c -o trialsByText.exe
+trialsByText.exe
