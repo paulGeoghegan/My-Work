35b03443914bfdbd216ffb700fd0232715807b0c
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-07-27 20:58:56 +0100

    Added item pick up functionality but it is not propperly working propperly yet

--- Personal Work/Game Programs/itemPickUp.c
+++ Personal Work/Game Programs/itemPickUp.c
@@ -4,6 +4,9 @@
 #include <stdbool.h>
 #include "structs.h"
 
+//Function signatures
+void itemList(short, short, short);
+
 //This function allows the user to pick up an item from the map
 void itemPickUp()
 {
@@ -14,6 +17,7 @@ void itemPickUp()
 
 	//Sets values for variables
 	itemToTake = 1;
+	invCheck = false;
 
 	//Checks if there is any items here
 	if(map[character.y][character.x].itemAmount > 0)
@@ -27,8 +31,8 @@ void itemPickUp()
 			printf("Which item do you want to pick up?\n");
 
 			//Sets values for min and max
-			min = 0;
-			max = map[character.y][character.x].itemAmount;
+			min = 1;
+			max = map[character.y][character.x].itemAmount+1;
 
 			//Prints out items
 			for(i = 0;i < map[character.y][character.x].itemAmount;i++)
@@ -48,18 +52,62 @@ void itemPickUp()
 		i = 0;
 
 		//checks if the user already has this item
-		while((invCheck == false) && (i < character.currentInventory))
+		while((invCheck == false) && (i <= character.currentInventory))
 		{
 
 			//Checks if items match
 			if(((map[character.y][character.x].item+(itemToTake-1))->id == inventory[i].id) && ((map[character.y][character.x].item+(itemToTake-1))->type == inventory[i].type))
 			{
 
-
+				//Changes value of invCheck
+				invCheck == true;
 
 			} //End if
+			else
+			{
+
+				//Adds 1 to i
+				i++;
+
+			} //End else
 
 		} //End while loop
+
+		//This checks if the user already has this item
+		if(invCheck == false)
+		{
+
+			//checks if theres space in the users inventory
+			if(character.currentInventory < inventorySize)
+			{
+
+				//Puts the item in the users inventory
+				itemList((map[character.y][character.x].item+(itemToTake-1))->type, (map[character.y][character.x].item+(itemToTake-1))->id, i);
+
+				//Sets item amount
+				inventory[i].amount = (map[character.y][character.x].item+(itemToTake-1))->amount;
+
+				//Increases value of currentInventory
+				character.currentInventory++;
+
+			} //End if
+			else
+			{
+
+				//Lets the user know that they have no space in there inventory
+				printf("There is not enough space in your inventory to pick up the item\n");
+				return;
+
+			} //End else
+
+		} //End if
+		else
+		{
+
+			//Adds item amount to inventory
+			inventory[i].amount = inventory[i].amount + (map[character.y][character.x].item+(itemToTake-1))->amount;
+
+		} //End else
 
 	} //End if
 	else
@@ -67,7 +115,11 @@ void itemPickUp()
 
 		//Tells the user that there is nothing to pick up
 		printf("There are no items here\n");
+		return;
 
 	} //End else
+
+	//Lets the user know what was added to there inventory
+	printf("%d %s Have been added to your inventory\n", (map[character.y][character.x].item+(itemToTake-1))->amount, inventory[i].name);
 
 } //End itemPickUp
