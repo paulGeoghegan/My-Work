63770582ae8c6411714c245a8729bca388be21fb
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-08-09 18:52:05 +0100

    Updated autoBanBot to have error checking

--- Personal Work/Bots/Twitch/autoBanBot/autoBanBot.js
+++ Personal Work/Bots/Twitch/autoBanBot/autoBanBot.js
@@ -4,14 +4,14 @@ const tmi = require('tmi.js');
 // Define configuration options
 const opts = {
 	identity: {
-		username: ''userName,
-		password: 'oauth:example'
+		username: 'userName',
+		password: 'oauth:1234abcd'
 	},
 
 	//List of channels
 	channels: [
-		'Channel1',
-		'Channel2'
+		'channel1',
+		'channel2'
 	] //End channel list
 }; //End opts
  
@@ -28,53 +28,66 @@ client.connect();
 // Called every time a message comes in
 function onMessageHandler (target, userstate, msg, self) {
 
-	// Ignore messages from the bot and non broadcasters
+	// Ignore messages from the bot and non moderators
 	if (self || userstate.username !== target.split('#').pop()) { return; }
 
 	//This splits up the command wherever there will be a space e.g. 1 command 2 userName 3 reason
 	const commandName = msg.split(' ');
 
 	//This will check which command the broadcaster wants to use by passing the first part of the commandName array to a switch case NOTE: the 0th element of the array should contain the command
-	switch(commandName[0]) {
+	switch(commandName[0].toLowerCase()) {
 
 		//Bans the user
-		case '!multiban':
+		case '!ban':
 
 			//Converts all elements after the 1st element in the commandName array to a string called reason
-			var reason = commandName.slice(2, commandName.length);
-			reason.join(' ');
+			const reason = commandName.slice(2, commandName.length).join(' ');
 
 			//This loop will ban the user in each chat
 			for(let i = 0;i < opts.channels.length;i++)
 			{
 
-				//Banns user from chat
-				client.ban(opts.channels[i], commandName[1], reason);
+				//Tries to ban user from channel
+				client.ban(opts.channels[i], commandName[1], reason).then(function() {
 
-				//This shows who has been banned and why NOTE: this is just for testing and will be removed
-				client.action(opts.channels[i], `${commandName[1]} has been banned for ${reason}`);
+					//Loggs command in console
+					console.log(`* Executed ${commandName[0]} command in ${opts.channels[i]} channel`);    
 
-				//Loggs command in console
-				console.log(`* Executed ${commandName[0]} command in ${opts.channels[i]} channel`);
+					//This shows who has been banned and why
+					client.action(opts.channels[i], `${commandName[1]} has been banned. Reason: ${reason}`);
+
+				}, function(err) {
+
+					//Logs error in console
+					console.log(err in ${opts.channels[i]});
+
+				}); //End error checking
 
 			} //End for
 
 			break;
 			//Unbanns a user
-		case '!unmultiban':
+		case '!unban':
 
 			//This loop will unban a user in all chats
 			for(let i = 0;i < opts.channels.length;i++)
 			{
 
 				//Unbanns user from chat
-				client.ban(opts.channels[i], commandName[1]);
+				client.unban(opts.channels[i], commandName[1]).then(function() {
 
-				//This shows who has been unbanned NOTE: this is just for testing and will be removed
-				client.action(opts.channels[i], `${commandName[1]} has been unbanned`);
+					//logs command on console
+					console.log(`* Executed ${commandName[0]} command in ${opts.channels[i]} channel`);
 
-				//logs command on console
-				console.log(`* Executed ${commandName[0]} command in ${opts.channels[i]} channel`);
+					//This shows who has been unbanned
+					client.action(opts.channels[i], `${commandName[1]} has been unbanned`);
+
+				}, function(err) {
+
+					//Logs error in console
+					console.log(err in ${opts.channels[i]});
+
+				}); //End error checking
 
 			} //End for
 
