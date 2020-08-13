86dc2672c12e2de4a40fe89ca571efe9d19d768b
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-08-13 20:17:41 +0100

    Drastic decrease in ram usage

--- Personal Work/Game Programs/trialsByText/structs.h
+++ Personal Work/Game Programs/trialsByText/structs.h
@@ -10,26 +10,26 @@
 struct playerCharacter
 {
 	char name[21];
-	short level;
-	int xp;
+	short level : 8;
+	short xp;
 	char *statNames[6];
 	short stats[6];
-	short x;
-	short y;
-	short currentInventory;
+	short x : 10;
+	short y : 10;
+	short currentInventory : 6;
 }; //End playerCharacter
 
 //This struct will store values for items
 struct items
 {
 	char name[21];
-	short id;
-	short type;
-	short amount;
+	short id : 10;
+	short type : 10;
+	short amount : 10;
 	char description[51];
-	short attack;
-	short defence;
-	short health;
+	short attack : 6;
+	short defence : 6;
+	short health : 7;
 	short value;
 }; //End item structure
 
@@ -37,18 +37,18 @@ struct items
 struct mapItem
 {
 	char name[21];
-	short id;
-	short type;
-	short amount;
+	short id : 10;
+	short type: 10;
+	short amount : 10;
 }; //End mapItem
 
 //This struct will be used for map generation and storage
 struct tile
 {
 	char name[21];
-	short id;
+	short id : 8;
 	char description[100];
-	short itemAmount;
+	short itemAmount : 6;
 	struct mapItem *item;
 }; //End tile struct
 
@@ -56,7 +56,7 @@ struct tile
 struct tileTemp
 {
 	char name[21];
-	short id;
+	short id : 8;
 	char description[desAmount][100];
 }; //End tileTemp struct
 
