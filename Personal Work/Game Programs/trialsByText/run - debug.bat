e956517378b43469f0a93374d0992727c848754d
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2020-08-21 12:18:52 +0100

    More debugging changes

--- Personal Work/Game Programs/trialsByText/run - debug.bat
+++ Personal Work/Game Programs/trialsByText/run - debug.bat
@@ -1,3 +1,3 @@
 @ECHO OFF 
-gcc admin.c character.c inventory.c itemPickUp.c items.c map.c menu.c play.c save.c tiles.c utility.c -g -o trialsByText.exe
-gdb trialsByText.exe
+gcc admin.c character.c inventory.c itemPickUp.c items.c map.c menu.c play.c save.c tiles.c utility.c -g -o trialsByText.out
+gdb trialsByText.out
